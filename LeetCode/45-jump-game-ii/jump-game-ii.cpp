04e3ccd6e1ebd9c3fea76432695e6944fb7fbe41
Anam <87643183+AnamMalikk@users.noreply.github.com>
2025-09-22 23:12:51 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (62.91%) - LeetSync

--- LeetCode/45-jump-game-ii/jump-game-ii.cpp
+++ LeetCode/45-jump-game-ii/jump-game-ii.cpp
@@ -1,60 +1,17 @@
 class Solution {
 public:
-// int solve(vector<int>& nums,int index,int jump){
-//     if(index==nums.size()-1) return jump;
-
-//     int mini=INT_MAX;
-//     for(int i=1;i<=nums[index];i++){
-//         mini=min(mini,solve(nums,index+i,jump+1));
-//     }
-// return mini;
-// }
-
-// int solve2(vector<int>& nums,int index,int jump,vector<vector<int>>& dp){
-
-//     if(index==nums.size()-1) return jump;
-// if(dp[index][jump]!=-1) return dp[index][jump];
-//     int mini=INT_MAX;
-//     for(int i=1;i<=nums[index];i++){
-//         if(index+i<nums.size())
-//         mini=min(mini,solve2(nums,index+i,jump+1,dp));
-//     }
-// return dp[index][jump]=mini;
-
-// }
-
-// int solve2(vector<int>& nums,int index,vector<int>& dp){
-// if(index>=nums.size()-1) return 0;
-
-// if(dp[index]!=-1) return dp[index];
-// int mini=INT_MAX;
-// for(int i=1;i<=nums[index];i++){
-//     int jump=solve2(nums,index+i,dp);
-//     if(jump!=INT_MAX) mini=min(mini,1+jump);
-// }
-// return dp[index]=mini;
-// }
-int solve3(vector<int>& nums) {
-    int n = nums.size();
-    vector<int> dp(n, INT_MAX);
-    dp[n-1] = 0;
-
-    for(int index = n - 2; index >= 0; index--) {
-        for(int i = 1; i <= nums[index]; i++) {
-            if(index + i < n && dp[index + i] != INT_MAX) {
-                dp[index] = min(dp[index], 1 + dp[index + i]);
+    int jump(vector<int>& nums) {
+        int jump = 0;
+        int reach = 0;
+        int last = 0;
+        int n = nums.size();
+        for(int i =0; i<n-1; i++){
+            reach = max(reach, nums[i]+i);
+            if(i == last){
+                last = reach;
+                jump++;
             }
         }
-    }
-
-    return dp[0];
-}
-
-
-    int jump(vector<int>& nums){
-        // vector<vector<int>> dp(nums.size()+1,vector<int>(nums.size()+1,-1));
-    //     vector<int> dp(nums.size()+1,-1);
-    // return solve2(nums,0,dp);
-return solve3(nums);
+        return jump;
     }
 };
