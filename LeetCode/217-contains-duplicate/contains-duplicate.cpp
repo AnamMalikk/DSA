522da65817f3c8e0b4c8581fa6dddd135036d41e
Anam <87643183+AnamMalikk@users.noreply.github.com>
2026-01-16 23:24:37 +0530

    Time: 43 ms (73.41%) | Memory: 90.9 MB (29.04%) - LeetSync

---
+++ LeetCode/217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,49 @@
+/*
+---------------Brute-----------------
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+*/
+
+/*
+------------------Better--------------------
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) return true;
+        }
+        return false;
+    }
+};
+
+*/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+
+        for (int num : nums) {
+            if (seen.count(num)) {
+                return true;
+            }
+            seen.insert(num);
+        }
+        return false;
+    }
+};
+
